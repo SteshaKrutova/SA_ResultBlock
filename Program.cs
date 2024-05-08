1666a5a668196c8c00e9156c9522a9bd44d1a7da
Krutova Stefania <mira.kru@yandex.ru>
2024-05-09 01:13:50 +0300

    did two functions

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+using System.Xml.XPath;
+
 Console.WriteLine("Hello, World!");
 // Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
 //длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
@@ -14,40 +17,96 @@ Console.WriteLine("Hello, World!");
 
 string[] array = [ "hv", "jjvfg","bfdjk","hfu" ];
 
-char[,] CreateResult{
-    char 
-    return array;
+string CreateArray() {
+    string NewElevent = Console.ReadLine();
+    string Array="";
+    while (NewElevent!=""){
+        Array+=NewElevent;
+        Array+=" ";
+        NewElevent = Console.ReadLine();
+    }
+    return Array;
 }
+string Array = CreateArray();
+Console.WriteLine(Array);
 
-int len0 = array.GetLength(0);
-Console.WriteLine(len0);
-for (int i=0;i<len0;i++){
-    string temp = array[i];
-    int lentemp = temp.Length;
-    for (int j=0;j<lentemp;j++){
-        Console.Write(temp[j]+" ");
- }
- Console.WriteLine();
- }
+string Three(string array){
+    string result = "";
+    string Tern = "";
+    int len = array.Length;
+    for (int i=0;i<len;i++){
+        if (array[i].ToString()!=" "){
+           Tern+=array[i];
+        }
+        else{
+            if (Tern.Length<=3){
+                result+=Tern;
+                result+=" ";
+                Tern="";
+            }
+                Tern="";
+        }
+    }
+    return result;
+}
+Console.WriteLine(Three(Array));
 
-string[] result = new string[len0];
-int t = 0;
-Console.WriteLine();
+// Console.WriteLine("Result: ");
+// //Console.WriteLine(Three(Array));
+// string array = "q ww eee rr ";
+// int len = array.Length;
+// string Tern ="";
+// string res ="";
+// for (int i=0;i<len;i++){
+//     if (array[i].ToString()!=" "){
+//         Tern+=array[i];
+//     }
+//     else{
+//         Console.WriteLine();
+//         Console.WriteLine(Tern.Length);
+//         if (Tern.Length==2){
+//             res+=Tern;
+//             Console.WriteLine(res);
+//         }
+//         res+=Tern;
+//         res+=" ";
+//         Console.Write("Res: ");
+//         Console.WriteLine(res);
+//         Tern="";
+        
+//     }
+// }
 
-for (int i=0;i<len0;i++){
-    string temp = array[i];
-    int lentemp = temp.Length;
-    if (lentemp<=3){
-        result[t]=temp;
-        t=t+1;
-    }   
- }
 
-Console.Write("[");
-int LenRes=result.Length;
-for (int i=0;i<t;i++){
-   Console.Write(result[i]); 
-   if (i!=t-1)
-    Console.Write(", "); 
- }
-Console.Write("]");
+// int len0 = array.GetLength(0);
+// Console.WriteLine(len0);
+// for (int i=0;i<len0;i++){
+//     string temp = array[i];
+//     int lentemp = temp.Length;
+//     for (int j=0;j<lentemp;j++){
+//         Console.Write(temp[j]+" ");
+//  }
+//  Console.WriteLine();
+//  }
+
+// string[] result = new string[len0];
+// int t = 0;
+// Console.WriteLine();
+
+// for (int i=0;i<len0;i++){
+//     string temp = array[i];
+//     int lentemp = temp.Length;
+//     if (lentemp<=3){
+//         result[t]=temp;
+//         t=t+1;
+//     }   
+//  }
+
+// Console.Write("[");
+// int LenRes=result.Length;
+// for (int i=0;i<t;i++){
+//    Console.Write(result[i]); 
+//    if (i!=t-1)
+//     Console.Write(", "); 
+//  }
+// Console.Write("]");
