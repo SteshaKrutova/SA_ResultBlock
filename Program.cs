3d0e48a59c57051771f0dc3c1c15995e28fbd48a
Krutova Stefania <mira.kru@yandex.ru>
2024-05-22 23:02:20 +0300

    wrote all function.it remains to write a file readme.md

--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿using System.Xml.XPath;
-
-Console.WriteLine("Hello, World!");
-// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
 //длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
 //либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 // лучше обойтись исключительно массивами.
@@ -18,35 +15,32 @@ string CreateArray() {
     string Array="";
     while (NewElevent!=""){
         Array+=NewElevent;
-        Array+=" ";
+        Array+="~";
         NewElevent = Console.ReadLine();
     }
     return Array;
 }
 
-string str = CreateArray();
+//функция преобразует строку в массив
+string[] StrToArray(string str){
+    int len = LenRes(str);
 
-// //функция определяет сколько в выходной строке элементов
-int len=1;
-int LenRes(string str){
+    int LenRes(string str){
+    int len=0;
     int i = 1;
     while (i<str.Length){
-        if (str[i].ToString()==" "){
+        if (str[i].ToString()=="~"){
             len = len+1;
         }
         i=i+1;
     }
     return len;
-}
-
-LenRes(str);
-string[] result = new string[len];
-
-string[] StrToArray(string str){
+    }
+    string[] result = new string[len];
     string Turn="";
     int Len = 0;
      for(int j=0;j<str.Length;j++){
-        if (str[j].ToString()==" "){
+        if (str[j].ToString()=="~"){
            result[Len]=Turn;
            Turn="";
            Len=Len+1;
@@ -61,22 +55,12 @@ string[] StrToArray(string str){
     }
     return result;
 }
- 
-StrToArray(str);
-
-// for (int i=0;i<result.GetLength(0);i++){
-//     Console.WriteLine(result[i]);
-// }
 
 //функция формирует выходную строку
-
-string[] array = StrToArray(str);
-
-
 string[] Three(string[] array){
     int len = 0;
     for (int i=0;i<array.GetLength(0);i++){
-        if (array[i].Length>=3){
+        if (array[i].Length<=3){
             len=len+1;
         }
     }
@@ -84,86 +68,52 @@ string[] Three(string[] array){
     string[] result = new string[len];
     int len1 = 0;
      for (int i=0;i<array.GetLength(0);i++){
-        if (array[i].Length>=3){
+        if (array[i].Length<=3){
             result[len1]=array[i];
             len1=len1+1;
         }
     }
 
-    for (int i=0;i<result.GetLength(0);i++){
-    Console.WriteLine(result[i]);
-    }
     return result;
 }
 
-Three(array);
+//функция красиво печатает массив
+void PrintArray(string[] result){
+    int len = result.GetLength(0);
+    Console.Write("[");
+    for (int i=0;i<len;i++){
+        if (i!=(len-1))
+            Console.Write("'"+result[i]+"'"+", ");
+        else
+            Console.Write("'"+result[i]+"'");
+    }
+    Console.Write("]");
+}
 
+//эта функция вызывет все остальные функции в нцжном порядке
+ void main(){
+    Console.WriteLine("Enter some values. After print each value press 'Enter'");
+    Console.WriteLine("Plese, don't use simbole '~'!!!");
+    Console.WriteLine("If you introdusted sufficent quantivalues values press 'Enter' whithout values!!!");
+    string str = CreateArray();
+    Console.Write("Your values: ");
+    PrintArray(StrToArray(str));
+    Console.WriteLine();
+    Console.Write("Your resul in if his data type an array: ");
+    PrintArray(Three(StrToArray(str)));
+ }
 
-// //функция формирует выходную строку
-// string Three(string array){
-//     string result = "";
-//     string Tern = "";
-//     int len = array.Length;
-//     for (int i=0;i<len;i++){
-//         if (array[i].ToString()!=" "){
-//            Tern+=array[i];
-//         }
-//         else{
-//             if (Tern.Length<=3){
-//                 result+=Tern;
-//                 result+=" ";
-//                 Tern="";
-//             }
-//                 Tern="";
-//         }
-//     }
-//     return result;
-// }
+main();
 
-
-
-// void PrintArray(string result){
-//     int len = result.Length;
-//     string Tern ="";
-//     Console.Write("[");
-//     for (int i=0;i<len;i++){
-//         if (result[i].ToString()!=" "){
-//            Tern+=result[i];
-//         }
-//         else{
-//             if (i!=Tern.Length){
-//                 Console.Write(",");
-//             }
-//             Console.Write("'"+Tern+"'");
-//             Tern="";
-//         }
-//     }
-//     Console.Write("]");
-// }
-
-// void main(){
+//  void main(){
 //     Console.WriteLine("Enter some values. After print each value press 'Enter'");
 //     Console.WriteLine("If you introdusted sufficent quantivalues values press 'Enter' whithout values!!!");
-//     string array = CreateArray();
-//     Console.WriteLine("Your values: "+array);
+//     string str = CreateArray();
+//     Console.Write("Your values: ");
+//     string[] result = StrToArray(str);
+//     PrintArray(result);
+//     Console.WriteLine();
 //     Console.Write("Your resul in if his data type an array: ");
-//     PrintArray(Three(array));
-// }
-
-// //main();
-
-// void ThreWithDouble(){
-//     string[] StrRes=new string[100];
-//     int i=0;
-//     string TempStr= Console.ReadLine();
-//     while(TempStr!=""){
-//         if (TempStr.Length<=3)
-//             StrRes[i]=TempStr;
-//             i=i+1;
-//         TempStr= Console.ReadLine();
-//     }
-//     for (int j=0;j<i;j++){
-//         Console.Write(StrRes[j]+" ");
-//     }
-// }
-// ThreWithDouble();
+//     string[] array =Three(result);
+//     PrintArray(array);
+//  }
